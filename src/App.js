6242b706fe0d514067757723158da5451a583526
Matt Carroll <matt.carroll139@gmail.com.au>
2018-07-20 00:15:31 +1000

    Step 5 # Included bootstrap and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 // @flow
 
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+//
+// STEP 5
+// using Create React App build a react application and update the UI using bootstrap
+//
 
-class App extends Component<{}> {
+import * as React from 'react'
+import { Button, Grid, Jumbotron, Navbar } from 'react-bootstrap'
+
+class App extends React.Component<{}> {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
+    return [
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">Home</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>,
+      <Grid fluid={false}>
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <p>
+            <Button bsStyle="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
+      </Grid>
+    ]
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // @flow
 
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
